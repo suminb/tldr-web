67e65429a5a922c8a95ef03b99435ab16b780922
Sumin Byeon <suminb@gmail.com>
2016-12-29 20:31:53 +0900

    Show an article

--- web/news/__init__.py
+++ web/news/__init__.py
@@ -13,3 +13,12 @@ news_module = Blueprint('news', __name__, template_folder='templates')
 @news_module.route('/')
 def news_home():
     return 'News home'
+
+
+@news_module.route('/<int:article_id>')
+def view_article(article_id):
+    article = Article.get_or_404(article_id)
+    context = {
+        'article': article,
+    }
+    return render_template('view_article.html', **context)
