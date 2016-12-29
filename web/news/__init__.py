73f3e156c7affaa0ccb6c2f7377813eb94fbd366
Sumin Byeon <suminb@gmail.com>
2016-12-29 20:31:33 +0900

    Import articles

--- web/news/__init__.py
+++ web/news/__init__.py
@@ -4,6 +4,8 @@ from flask import Blueprint, render_template, request
 import requests
 from werkzeug.exceptions import BadRequest
 
+from web.news.models import Article
+
 
 news_module = Blueprint('news', __name__, template_folder='templates')
 
