d4616061e450ce93acc3352e87be43b45884042d
Sumin Byeon <suminb@gmail.com>
2017-05-08 09:55:15 +0900

    Use Vue for text-summary

--- web/static/main.js
+++ web/static/main.js
@@ -1,10 +1,3 @@
-var TextSummaryModel = Backbone.Model.extend({
-  state: 'ready', // ready | loading | errored
-  text: '',
-  summary: ''
-});
-
-
 var URLSummaryModel = Backbone.Model.extend({
   fetch: function(url) {
     var model = this;
@@ -20,36 +13,6 @@ var URLSummaryModel = Backbone.Model.extend({
     .fail(function(resp) {
       // TODO: Do something
     });
-  }
-});
-
-
-var TextSummaryView = Backbone.View.extend({
-  el: '#text-summary',
-  bindings: {
-  },
-
-  events: {
-    'change textarea': 'textareaChanged',
-    'change:state': 'stateChanged'
-  },
-
-  initialize: function() {
-    console.log('view.initialize');
-    this.textareaChanged();
-  },
-
-  render: function() {
-    console.log('view.render');
-  },
-
-  textareaChanged: function() {
-    // FIXME: Any more robust way to handle this?
-    this.model.text = $('textarea[name=text]').val();
-  },
-
-  stateChanged: function(state) {
-    console.log('stateChanged');
   }
 });
 
@@ -80,19 +43,6 @@ var URLSummaryView = Backbone.View.extend({
 });
 
 
-$('#text-summary form').on('submit', function(event) {
-  event.preventDefault();
-
-  var action = event.target.action;
-  var text = textSummaryModel.text;
-  textSummaryModel.set({state: 'loading'});
-
-  $.post(action, {text: text}, function(resp) {
-   textSummaryModel.set({summary: resp, state: 'ready'});
-  }, 'text');
-});
-
-
 $('#url-summary form').on('submit', function(event) {
   event.preventDefault();
 
@@ -110,44 +60,8 @@ $('#url-summary form').on('submit', function(event) {
 });
 
 
-window.textSummaryModel = new TextSummaryModel();
 window.urlSummaryModel = new URLSummaryModel();
 
-
-// TODO: Could we remove this boiler-plate code?
-textSummaryModel.on('change:summary', function(m, v) {
-  console.log('model.summary changed');
-});
-
-textSummaryModel.on('change:state', function(m, state) {
-
-  console.log('model.state changed:', state);
-
-  // NOTE: At this point, the `model` sure looks like SummaryModel,
-  // but it does not have `stateChanged` function defined inside the mode.
-  // So we decided to do the work here until we figure what exactly is going
-  // on.
-  //
-  // textSummaryModel.stateChanged(state);
-
-  if (state == 'ready') {
-    var summary = textSummaryModel.get('summary');
-    $('#loading').hide();
-    if (summary) {
-      // TODO: Bind this view to textSummaryModel.summary
-      $('#summary').html(summary).show();
-    }
-    else
-      $('#summary').hide();
-  }
-  else if (state == 'loading') {
-    $('#loading').show();
-    $('#summary').hide();
-  }
-  else {
-    throw 'Invalid state: ' + state;
-  }
-});
 
 urlSummaryModel.on('change:html', function(model, value) {
   if (value) {
@@ -184,13 +98,4 @@ urlSummaryModel.on('change:summary', function(mode, value) {
 
 
 
-var textSummaryView = new TextSummaryView({model: textSummaryModel});
 var urlSummaryView = new URLSummaryView({model: urlSummaryModel});
-
-$(function() {
-  textSummaryModel.set({state: 'ready'});
-
-  // FIXME: Move this elsewhere
-  $('.ui.progress').hide();
-  $('.ui.summary').hide();
-});
